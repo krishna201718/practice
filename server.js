237eb45d52ee4c9f3a66436dff225a9689648a0a
krishna201710 <krishna201710@gmail.com>
2021-05-03 15:41:11 +0530

    num add

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+const add =  require('./lib.js');
+
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
+  res.write(add(2,3)); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
