96945037c3903f2a02db784002e916909d0528ac
krishna201710 <krishna201710@gmail.com>
2021-05-06 09:50:44 +0530

    require wrong info

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const assert = require('assert/')
+const assert = require('assert')
 const add = require('./src/lib')
 
-it('adding 2 and 4', () =>{
+it('Adding 2 and 4', () => {
     assert.equal(add(2, 4), 6)
 })
